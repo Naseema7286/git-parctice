50ea48c40461ceb04d077367edf74a52204475c5
Naseema <naseemasayyad33@gmail.com>
2025-02-16 09:33:47 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from google.cloud import compute_v1
+boto 4
+
+def list_gcp_regions():
+    client = compute_v1.RegionsClient()
+    project = "nareshit"  # Replace with your project ID
+
+    request = compute_v1.ListRegionsRequest(project=project)
+    response = client.list(request=request)
+
+    print("Available multi Cloud Regions:")
+    for region in response:
+        print(f"- {region.name} (Status: {region.status})")
+
+if __name__ == "__main__":
+    list_gcp_regions()
